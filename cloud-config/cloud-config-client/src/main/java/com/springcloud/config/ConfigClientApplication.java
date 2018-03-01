725df95ba2fffd058e8794efe63080be81254db0
chuantouchi_lee <chuantouchi_lee@hotmail.com>
2018-03-01 13:53:15 +0800

    测试bus，增加刷新全局@RefreshScope注解

--- cloud-config/cloud-config-client/src/main/java/com/springcloud/config/ConfigClientApplication.java
+++ cloud-config/cloud-config-client/src/main/java/com/springcloud/config/ConfigClientApplication.java
@@ -3,11 +3,13 @@ package com.springcloud.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RefreshScope // 刷新全局
 public class ConfigClientApplication {
 	
 	@Autowired
